490cce1052ad7666c08ace63cebbbd1c53c9e941
Gain John <emperorduke5@gmail.com>
2020-12-08 08:11:28 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+
+// REDUX
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// import store from './redux/store';
+
+ReactDOM.render(
+
+  <Provider store={createStoreWithMiddleware(reducers)}>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
